74977921d35107945df8b643ef9b490c9c1c35ce
Ali Madooei <madooei@jhu.edu>
2026-02-11 11:27:04 -0500

    snake-01: Set up game board with CSS grid

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+
+const GRID_SIZE = 20;
+
+const board = document.getElementById("board");
+
+for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  board.appendChild(cell);
+}
